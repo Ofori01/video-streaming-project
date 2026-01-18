afa539393c570f7b1139d0458bbc02f79e15bcd0
Ofori01 <oforidarkwah7@gmail.com>
2026-01-18 11:41:52 +0000

    frontend: enforced role checking before loading admin layout

--- frontend/src/layouts/AdminLayout.tsx
+++ frontend/src/layouts/AdminLayout.tsx
@@ -20,72 +20,72 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { Toaster } from "sonner";
 
 const AdminLayout: React.FC = () => {
-  const [isAuthenticated, userRole] = useSelector((state: RootState) => [
-    state.auth.isAuthenticated,
-    state.auth.role,
-  ]);
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated,
+  );
+  const userRole = useSelector((state: RootState) => state.auth.role);
 
   const location = useLocation();
   const pathSegments = location.pathname.split("/").filter(Boolean);
 
   return (
-    <div className="relative">
+    <div className="relative scroll-smooth">
       <main>
         <Toaster position="top-right" />
-        {/* {isAuthenticated ? (
-          userRole === USER_ROLE.USER ? (
-            <Navigate to="/" />
+        {isAuthenticated ? (
+          userRole === USER_ROLE.ADMIN ? (
+            <SidebarProvider>
+              <AppSidebar />
+              <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 fixed bg-transparent backdrop-blur-3xl w-full z-9999">
+                  <div className="flex items-center gap-2 px-4 ">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator
+                      orientation="vertical"
+                      className="mr-2 data-[orientation=vertical]:h-4"
+                    />
+                    <Breadcrumb>
+                      <BreadcrumbList>
+                        {pathSegments.map((segment, index) => {
+                          // Build the URL for each breadcrumb link
+                          const href = `/${pathSegments
+                            .slice(0, index + 1)
+                            .join("/")}`;
+                          const isLast = index === pathSegments.length - 1;
+
+                          return (
+                            <React.Fragment key={href}>
+                              <BreadcrumbItem>
+                                {isLast ? (
+                                  <span className="font-bold text-primary">
+                                    {segment}
+                                  </span>
+                                ) : (
+                                  <BreadcrumbLink href={href}>
+                                    {segment}
+                                  </BreadcrumbLink>
+                                )}
+                              </BreadcrumbItem>
+                              {!isLast && <BreadcrumbSeparator />}
+                            </React.Fragment>
+                          );
+                        })}
+                      </BreadcrumbList>
+                    </Breadcrumb>
+                  </div>
+                </header>
+                {/* contents */}
+                <div className="pt-16 group-has-data-[collapsible=icon]/sidebar-wrapper:pt-12 transition-all ease-linear">
+                  <Outlet />
+                </div>
+              </SidebarInset>
+            </SidebarProvider>
           ) : (
-            <Outlet />
+            <Navigate to="/" />
           )
         ) : (
-          <Navigate to="/login" />
-        )} */}
-        <SidebarProvider>
-          <AppSidebar />
-          <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2 border-b-2 border-destructive/30 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-              <div className="flex items-center gap-2 px-4 ">
-                <SidebarTrigger className="-ml-1" />
-                <Separator
-                  orientation="vertical"
-                  className="mr-2 data-[orientation=vertical]:h-4"
-                />
-                <Breadcrumb>
-                  <BreadcrumbList>
-                    {pathSegments.map((segment, index) => {
-                      // Build the URL for each breadcrumb link
-                      const href = `/${pathSegments
-                        .slice(0, index + 1)
-                        .join("/")}`;
-                      const isLast = index === pathSegments.length - 1;
-
-                      return (
-                        <React.Fragment key={href}>
-                          <BreadcrumbItem>
-                            {isLast ? (
-                              <span className="font-bold text-primary">
-                                {segment}
-                              </span>
-                            ) : (
-                              <BreadcrumbLink href={href}>
-                                {segment}
-                              </BreadcrumbLink>
-                            )}
-                          </BreadcrumbItem>
-                          {!isLast && <BreadcrumbSeparator />}
-                        </React.Fragment>
-                      );
-                    })}
-                  </BreadcrumbList>
-                </Breadcrumb>
-              </div>
-            </header>
-            {/* contents */}
-
-            <Outlet />
-          </SidebarInset>
-        </SidebarProvider>
+          <Navigate to="/" />
+        )}
       </main>
     </div>
   );
