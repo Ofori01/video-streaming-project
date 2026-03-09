67ca81ed686977d7b19db2adbee8de25302bdb1a
Ofori01 <oforidarkwah7@gmail.com>
2026-03-09 14:45:45 +0000

    feat(backend): implement GetVideoComments, GetCommentReplies, UpdateComment and DeleteComment in CommentService

--- backend/src/services/CommentService.ts
+++ backend/src/services/CommentService.ts
@@ -5,11 +5,13 @@ import { VideoEntity } from "../entities/VideoEntity";
 import { CreateVideoCommentBodyDto } from "../interfaces/dtos/comment-dto";
 import { ICommentsRepository } from "../interfaces/repositories/ICommentsRepository";
 import { ICommentService } from "../interfaces/services/ICommentService";
+import { USER_ROLE } from "../lib/types/common/enums";
+import { ForbiddenError } from "../middlewares/errorHandler/errors/ForbiddenError";
 import { NotFoundError } from "../middlewares/errorHandler/errors/NotFoundError";
 import { UnauthorizedError } from "../middlewares/errorHandler/errors/UnauthorizedError";
 import { GenericService } from "./GenericService";
 
-class CommentService
+export class CommentService
   extends GenericService<CommentEntity>
   implements ICommentService
 {
@@ -65,5 +67,49 @@ class CommentService
 
       return comment;
     });
+  };
+
+  GetVideoComments = async (videoId: number, page: number, limit: number) => {
+    return this.commentRepository.GetVideoComments(videoId, page, limit);
+  };
+
+  GetCommentReplies = async (commentId: number) => {
+    return this.commentRepository.GetCommentReplies(commentId);
+  };
+
+  UpdateComment = async (
+    commentId: number,
+    userId: number,
+    content: string,
+  ) => {
+    const comment = await this.commentRepository.GetOne({
+      where: { id: commentId },
+      relations: { createdBy: true },
+    });
+    if (!comment) {
+      throw new NotFoundError("Comment not found");
+    }
+    if (comment.createdBy.id !== userId) {
+      throw new ForbiddenError("You are not allowed to update this comment");
+    }
+    return this.commentRepository.UpdateCommentContent(commentId, content);
+  };
+
+  DeleteComment = async (
+    commentId: number,
+    userId: number,
+    userRole: USER_ROLE,
+  ) => {
+    const comment = await this.commentRepository.GetOne({
+      where: { id: commentId },
+      relations: { createdBy: true },
+    });
+    if (!comment) {
+      throw new NotFoundError("Comment not found");
+    }
+    if (comment.createdBy.id !== userId && userRole !== USER_ROLE.ADMIN) {
+      throw new ForbiddenError("You are not allowed to delete this comment");
+    }
+    return this.commentRepository.Delete(commentId);
   };
 }
