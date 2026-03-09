381a1e404b28d5ef0b117ed8d40c95bf8785cab2
Ofori01 <oforidarkwah7@gmail.com>
2026-03-09 15:20:33 +0000

    feat(frontend): add comment service and comment API endpoints

--- frontend/src/backend/constants.ts
+++ frontend/src/backend/constants.ts
@@ -5,7 +5,6 @@ export const endpoints = {
   signUp: "/auth/sign-up",
   availableRoles: "auth/roles",
 
-
   // videos
   getAllVideos: "/video",
   getVideo: (videoId: number) => `/video/${videoId}`,
@@ -14,6 +13,16 @@ export const endpoints = {
 
   // dashboard
   getDashboardStats: "/video/stats",
+
+  // comments
+  getVideoComments: (videoId: number) => `/comments/${videoId}`,
+  getCommentReplies: (videoId: number, commentId: number) =>
+    `/comments/${videoId}/${commentId}/replies`,
+  createComment: (videoId: number) => `/comments/${videoId}`,
+  updateComment: (videoId: number, commentId: number) =>
+    `/comments/${videoId}/${commentId}`,
+  deleteComment: (videoId: number, commentId: number) =>
+    `/comments/${videoId}/${commentId}`,
 
   // SSE
   uploadProgressSse: (videoId: number) => `/sse/${videoId}`,
